1d9f7d4adf3d58856cb5e134302027e166713301
GordonDiggs <gordon@gordondiggs.com>
2014-06-29 14:29:53 -0400

    Start mechanism of updating sites

--- changes.rb
+++ changes.rb
@@ -2,12 +2,40 @@ require 'bundler'
 Bundler.require
 
 configure do
-  enable :sessions
+  require_relative 'site'
+  DataMapper.finalize
 
-  uri = URI.parse(ENV["REDISCLOUD_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  enable :sessions
+end
+
+configure :production do
+  DataMapper::Logger.new($stdout, :info)
+end
+
+configure :test do
+  ENV['DATABASE_URL'] = 'postgres://localhost/changes_test'
+  DataMapper::Logger.new($stdout, :error)
+end
+
+configure :development do
+  ENV['DATABASE_URL'] = 'postgres://localhost/changes'
+  DataMapper::Logger.new($stdout, :debug)
+end
+
+configure do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+  DataMapper.auto_upgrade!
 end
 
 get '/' do
   haml :index
+end
+
+post '/sites' do
+  @site = Site.new(params['site'])
+  if @site.save
+    redirect '/'
+  else
+    raise
+  end
 end
--- site.rb
+++ site.rb
@@ -1,14 +1,29 @@
 class Site
-  include HTTParty
+  include DataMapper::Resource
 
-  attr_accessor :url, :content
+  # set up the schema for the item
+  property :id, Serial
+  property :created_at, DateTime
+  property :updated_at, DateTime
+  property :title, String, lazy: false
+  property :url, String, lazy: false, unique: true
+  property :content, Text, lazy: false
+
+  validates_presence_of :url, :title
+
+  after :create, :get_new_content
 
   def get_new_content
-    response = self.class.get(url)
+    response = HTTParty.get(url)
     if response.code == 200
-      self.content = response.body
+      new_content = response.body
+      if new_content != self.content
+        # send email of changed content with diff
+        self.content = new_content
+        self.save!
+      end
     else
-      # do something because it's bad
+      # send email of error getting url
     end
   end
 
